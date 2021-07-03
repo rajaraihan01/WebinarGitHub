ca35bda20ad77e7ba87b7b74f33d5759670ecfb8
rajaraihan01 <85173860+rajaraihan01@users.noreply.github.com>
2021-07-03 16:44:41 +0700

    Add files via upload

---
+++ Gitwebinar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitwebinar;
+
+/**
+ *
+ * @author HP
+ */
+public class Gitwebinar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+<<<<<<< HEAD
+        System.out.println("Ini branch2");
+=======
+        System.out.println("Ini branch1");
+>>>>>>> branch1
+    }
+    
+}
